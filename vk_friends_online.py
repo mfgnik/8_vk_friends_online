868356ee035f728b84f08482d1a0f29e2321e116
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-03-19 20:39:17 +0300

    Update vk_friends_online.py

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,15 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6391427
 
 
 def get_user_login():
-    pass
+    return input('Введите логин: ')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass('Введите пароль: ')
 
 
 def get_online_friends(login, password):
@@ -17,16 +17,27 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    api_connection_context = vk.API(session)
+    online_friends_id_list = api_connection_context.friends.getOnline(
+        v=5.52
+    )
+    online_friends_list = api_connection_context.users.get(
+        user_ids=online_friends_id_list, v=5.52
+    )
+    return online_friends_list
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print(friend['first_name'], friend['last_name'])
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    try:
+        friends_online_list = get_online_friends(login, password)
+        output_friends_to_console(friends_online_list)
+    except ValueError:
+        print("Указаны неверные логин/пароль.")
